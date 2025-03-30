aa9f486d490b4fe71351a3df812f3d0601ad3374
Nazek Almalaika Altayeb <n.almalaika@yahoo.com>
2025-03-30 22:21:16 +0200

    add stop watch

--- Quiz/src/app/quiz/quiz.component.ts
+++ Quiz/src/app/quiz/quiz.component.ts
@@ -3,6 +3,8 @@ import { Component, ViewContainerRef } from '@angular/core';
 import { QuizService } from '../quiz.service';
 import {Quiz} from './quiz';
 import { QuestionsComponent } from '../questions/questions.component';
+import { Time } from '@angular/common';
+import { Timestamp } from 'rxjs';
 
 
 @Component({
@@ -14,22 +16,44 @@ export class QuizComponent {
 
   public quiz: Quiz;
   public category: string = " ";
+  public  amountOfQuestions: any;
   questions_answers_area: any;
+  public intervalId : any;
+  public quizDuration :any;
+  public minutes : any;
+  public seconds : any;
+
   constructor(private quizService: QuizService, private viewContainer: ViewContainerRef){
     this.quiz = new Quiz();
     
     console.log(this.questions_answers_area);
 
   }
+
   onQuizCreate(quiz:Quiz){
     console.log(quiz);
     this.quizService.postQuizdetails(quiz);   
     this.category = quiz.category.toString();
-  
+    this.amountOfQuestions = quiz.amountOfQuestions;
+    this.quizDuration = this.amountOfQuestions*60;   
+    
   }
+
   startQuiz(){
     this.viewContainer.createComponent(QuestionsComponent);
-   
+    /** set Interval Id */
+    this.intervalId = setInterval(() => {
+      this.amountOfQuestions;
+    }, 1000);
+  }
+
+  startCountDown() {
+    this.minutes = Math.floor(this.quizDuration / 60);
+    this.seconds = this.quizDuration % 60;
+  }
+
+  stopCountDown() {
+    clearInterval(this.intervalId);
   }
   
  
